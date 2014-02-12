21e8ca3bd4212293c3f95b5746909c98cc00a555
gskielian <gregory.kielian@gmail.com>
2014-02-12 12:51:34 -0800

    created a test method which goes north

--- examples/square-path.ino
+++ examples/square-path.ino
@@ -24,51 +24,28 @@ Robox pika(9,10);
 
 void setup()
 {
+  //begin I2C Communication
   Wire.begin();
-  
+
+  //initialize magnets
   mag.initialize();
 
-  
+  //attach servo motors
   pika.attach();
+
+  //begin serial interface
   Serial.begin(115200);
 }
 
 void loop()
 {
 
-  mag.getHeading(&mx, &my, &mz);
-  
-  // To calculate heading in degrees. 0 degree indicates North
-  float heading = atan2(my, mx);
-  if(heading < 0)
-    heading += 2 * M_PI;
-  Serial.print("heading:\t");
-  Serial.println(heading * 180/M_PI);
 
-  //TODO create a Robox method which turns UNTIL an angle
-  
-  //idea 1: perhaps we can just slow down until we reach that angle (a perfect one shot)
-  
-  //idea 2: may need to calibrate for different speeds
-  
-  //TODO create a Robox method which uses true north to calibrate wheels
-  //to go straight when doing a forwards or backwards method
-  
-  //idea 1: a single speed
-  
-  //idea 2: 3 speeds (crawl, walk, run);
-  
-  
-  //TODO overload the forward left and right methods to optionally accept
-  //an integer for the speed in that direction, this might complicate the calibration
-  //though as we will need to test forward and backward, but also for speed space.
-  //implementation idea: have it go forwards and backwards plenty of times.
-  //implementation idea: perhaps have it write to eeprom just a little in order
-  //to save the calibration settings
-  
-  
- 
-  Serial.println("pika.forward();");
+  //make the robot point north
+  Serial.println("pika, go north");
+  goNorth(); //TODO make a goNorth method in Pikachu
+  delay(2000);
+
   pika.forward();
   delay(2000);
 
@@ -89,3 +66,33 @@ void loop()
   delay(2000);
 
 }
+
+//pika.goNorth();
+
+void goNorth() {
+  //get current bearing
+  mag.getHeading(&mx, &my, &mz);
+  float heading = atan2(my, mx)a * 180/M_PI;
+
+  while ( target_bearing - current_bearing > 1 || target_bearing - current_bearing < -1 ) {
+
+    if ( target_bearing > current_bearing ) {
+      pika.right();
+    } else if ( target_bearing < current_bearing ) {
+      pika.left();
+    }
+
+    //get current bearing
+    mag.getHeading(&mx, &my, &mz);
+    float heading = atan2(my, mx)a * 180/M_PI;
+
+    delay(10); // we may want to play around with this
+  }
+
+}
+
+//  //ooo pointers
+//
+//  // To calculate heading in degrees. 0 degree indicates North
+//  Serial.print("heading:\t");
+//  Serial.println(heading * 180/M_PI);
